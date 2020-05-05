e16911c364929db63d8598990c14ac9f0918b54f
subhra74 <subhradasgupta64@gmaii.com>
2020-05-05 11:05:56 +0200

    proxy detection and certificate check

--- muon-app/src/main/java/muon/app/updater/UpdateChecker.java
+++ muon-app/src/main/java/muon/app/updater/UpdateChecker.java
@@ -1,18 +1,29 @@
 package muon.app.updater;
 
+import java.net.ProxySelector;
 import java.net.URL;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.markusbernhardt.proxy.ProxySearch;
 
 import muon.app.App;
 
 public class UpdateChecker {
 	public static final String UPDATE_URL = "https://api.github.com/repos/subhra74/snowflake/releases/latest";
 
+	static {
+		CertificateValidator.registerCertificateHook();
+	}
+
 	public static boolean isNewUpdateAvailable() {
 		try {
+			ProxySearch proxySearch = ProxySearch.getDefaultProxySearch();
+			ProxySelector myProxySelector = proxySearch.getProxySelector();
+
+			ProxySelector.setDefault(myProxySelector);
+
 			System.out.println("Checking for url");
 			ObjectMapper objectMapper = new ObjectMapper();
 			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
